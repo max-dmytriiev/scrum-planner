800aa636fb1c4029b6b7b7ea03c5ca4b750aef3a
Maksym Dmytriiev <maksym_dmytriiev@epam.com>
2019-09-02 17:31:47 +0300

    SCPL-0005: Add basic application starter and ping endpoint; Add support for Maven Polyglot

---
+++ backend-parent/api/src/main/java/ua/in/soul/tools/scrum/api/HealthCheckController.java
@@ -0,0 +1,13 @@
+package ua.in.soul.tools.scrum.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("service")
+public class HealthCheckController {
+
+    @GetMapping("ping")
+    public String ping() {
+        return "OK";
+    }
+}
---
+++ backend-parent/starter/src/main/java/ua/in/soul/tools/scrum/PlannerBackendApplication.java
@@ -0,0 +1,11 @@
+package ua.in.soul.tools.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerBackendApplication extends SpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerBackendApplication.class, args);
+    }
+}
